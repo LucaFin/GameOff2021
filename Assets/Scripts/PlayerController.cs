9d1880448812ed1f04b3294e4d61bfee2514ef94
LucaFin <luca.finoia@gmail.com>
2021-11-03 22:38:18 +0100

    first player movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,6 +7,8 @@ public class PlayerController : MonoBehaviour
     private CharacterController2D controller;
     [SerializeField]
     private float force;
+    private float inputValue;
+    private bool jump;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,6 +18,13 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        controller.Move(force,false,false);
+        inputValue = Input.GetAxisRaw("Horizontal");
+        jump = Input.GetButtonDown("Jump");
+    }
+
+    private void FixedUpdate()
+    {
+        controller.Move(inputValue*force*Time.deltaTime, false, jump);
+        jump = false;
     }
 }
