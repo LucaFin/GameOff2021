b4a62ed36a4b9e0bd5ab31af715f16fd3b3ad071
MaicolCero <maicol.cerofolini@gmail.com>
2021-11-25 18:34:46 +0100

    up

--- Assets/Scripts/IntroTerminal.cs
+++ Assets/Scripts/IntroTerminal.cs
@@ -6,14 +6,36 @@ public class IntroTerminal : MonoBehaviour
 {
 
     //Game state
+    string input, input1;
     enum Screen { MainMenu, Start, BUG };
     Screen currentScreen;
 
     // Start is called before the first frame update
     void Start()
     {
+        input1 = ("Questo e un testo di prova");
+        ShowStory(input1);
         ShowMainMenu();
     }
+
+
+    //TEST
+    //
+
+    private void ShowStory(string s)
+    {
+        foreach (char c in s)
+        {
+            Terminal.WriteLine(c.ToString());
+            for (float i = 0; i < 1; i += Time.deltaTime)
+            {
+            }
+            
+        }
+    }
+
+    //
+    //
 
     void ShowMainMenu() //Funzione per visualizzare il terminale   
     {
@@ -21,4 +43,16 @@ public class IntroTerminal : MonoBehaviour
         Terminal.WriteLine("Welcome to ");
     }
 
+    void OnUserInput(string input)
+    {
+        input = input.ToLower();
+        if (input == "menu")
+        {
+            ShowMainMenu();
+        }
+        else if (input == "show")
+        {
+            ShowStory(input1);
+        }
+    }
 }
