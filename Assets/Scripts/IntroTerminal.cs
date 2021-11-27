948e20a5eb5e74571dda797c4eba3b38ed31b7ed
MaicolCero <maicol.cerofolini@gmail.com>
2021-11-27 22:10:25 +0100

    upIntro

--- Assets/Scripts/IntroTerminal.cs
+++ Assets/Scripts/IntroTerminal.cs
@@ -9,6 +9,7 @@ public class IntroTerminal : MonoBehaviour
     //Game state
     string input;
     [SerializeField] Terminal connectedToTerminal;
+    [SerializeField] float terminalSpeed=0.05f;
     enum Screen { MainMenu, Start, BUG };
     Screen currentScreen;
 
@@ -16,13 +17,12 @@ public class IntroTerminal : MonoBehaviour
     void Start()
     {
         ShowMainMenu();
-        ShowStory("Main Menu" + '\n' + "Write space to Play" + '\n');
-        Keyboard.canWrite = true;
     }
 
     void ShowMainMenu() //Funzione per visualizzare il terminale   
     {
         currentScreen = Screen.MainMenu;
+        Terminal.WriteLine("\n");
         Terminal.WriteLine(@"
                   \_/
                  (* *)
@@ -33,6 +33,7 @@ public class IntroTerminal : MonoBehaviour
                 _(___)_
     Welcome to [-] a [-] MazeBug game
   ");
+        ShowStory("\n\n\n\n\n\n\n\n\n\n Main Menu" + '\n' + "0. Write 'start' to play;" + '\n' + "1. Write 'menu' to reload main menu;" + '\n' + "2. Write 'story' to read story;" + '\n' + "3. Write 'quit' to close the game;" + '\n');
     }
 
     private void ShowStory(string s)
@@ -45,12 +46,14 @@ public class IntroTerminal : MonoBehaviour
 
     IEnumerator Story(string s)
     {
+        Keyboard.canWrite = false;
         yield return new WaitForSeconds(2f);
         foreach (char c in s)
         {
             connectedToTerminal.ReceiveFrameInput(c.ToString());
-            yield return new WaitForSeconds(0.1f);
+            yield return new WaitForSeconds(terminalSpeed);
         }
+        Keyboard.canWrite = true;
     }
 
     void OnUserInput(string input)
@@ -60,11 +63,35 @@ public class IntroTerminal : MonoBehaviour
             input = input.ToLower();
             if (input == "menu")
             {
+                Keyboard.canWrite = false;
                 ShowMainMenu();
+                Keyboard.canWrite = true;
             }
-            if (input == "space")
+            if (input == "start")
             {
                 SceneManager.LoadScene(1);
+            }
+            if (input == "story")
+            {
+                Keyboard.canWrite = false;
+                Terminal.WriteLine("\n\n\n\n\n\n\n\n\n\n\n");
+                ShowStory(@"The story behind the scene
+An artificial intelligence wants to    materialize in the real world but to do so it needs the prison of circuits that surrounds it to be destroyed, so it     creates a virus that will have to be    guided in its mission through amazing   evolutions.
+
+One of its forms will be:              'First actual case of bug being found'               Note from Lt.Grace Hopper
+Who knows what shape it is...         
+Write 'menu' to return or 'story' to   repeat.
+");
+
+                Keyboard.canWrite = true;
+            }
+            if (input == "quit")
+            {
+                Application.Quit();
+            }
+            if (input == "moth")
+            {
+                SceneManager.LoadScene(2);
             }
         }
 
