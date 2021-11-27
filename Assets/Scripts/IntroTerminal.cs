ee2424b909c680febb8ed1d3f8f0d2db6bd97bf8
MaicolCero <maicol.cerofolini@gmail.com>
2021-11-27 16:58:19 +0100

    up

--- Assets/Scripts/IntroTerminal.cs
+++ Assets/Scripts/IntroTerminal.cs
@@ -6,7 +6,7 @@ public class IntroTerminal : MonoBehaviour
 {
 
     //Game state
-    string input, input1;
+    string input;
     [SerializeField] Terminal connectedToTerminal;
     enum Screen { MainMenu, Start, BUG };
     Screen currentScreen;
@@ -14,52 +14,36 @@ public class IntroTerminal : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        input1 = ("Questo e un testo di prova");
-        ShowStory(input1);
         ShowMainMenu();
-    }
-
-
-    //TEST
-    //
-
-    private void ShowStory(string s)
-    {
-        string ns = s + '\n';
-
-        StartCoroutine(Story(s));
-
-    }
-
-    IEnumerator Story(string s)
-    {
-        foreach (char c in s)
-        {
-            connectedToTerminal.ReceiveFrameInput(c.ToString());
-            yield return new WaitForSeconds(0.1f);
-        }
         Keyboard.canWrite = true;
     }
-
-    //
-    //
 
     void ShowMainMenu() //Funzione per visualizzare il terminale   
     {
         currentScreen = Screen.MainMenu;
-        Terminal.WriteLine("Welcome to ");
+        Terminal.WriteLine("Welcome to aMazeBug game..");
+        Terminal.WriteLine(@"
+                  \_/
+                 (* *)
+                __)#(__
+               ( )...( )(_)
+               || |_| ||//
+            >==() | | ()/
+                _(___)_
+               [-]   [-]
+  ");
     }
 
     void OnUserInput(string input)
     {
-        input = input.ToLower();
-        if (input == "menu")
+        if (input != null)
         {
-            ShowMainMenu();
+            input = input.ToLower();
+            if (input == "menu")
+            {
+                ShowMainMenu();
+            }
         }
-        else if (input == "show")
-        {
-            ShowStory(input1);
-        }
+
     }
 }
