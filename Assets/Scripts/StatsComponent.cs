f5370b29999f5b6de9de8182b237e72003b757b0
LucaFin <luca.finoia@gmail.com>
2021-11-30 18:57:10 +0100

    Component

--- Assets/Scripts/StatsComponent.cs
+++ Assets/Scripts/StatsComponent.cs
@@ -4,13 +4,26 @@ using UnityEngine;
 
 public class StatsComponent : Stats
 {
+    [SerializeField]
+    GameObject explosion;
     public override void InflictDamage(float damage)
     {
         life -= damage;
         if (life <= 0)
         {
-            this.gameObject.SetActive(false);
-            ComponentHandler.componentHandler.Unlock();
+            explosion.SetActive(true);
+            explosion.GetComponent<Animator>().Play("EnemyExplosionLv1");
+            Invoke("Disable",0.4f);
         }
+    }
+
+    public void Disable()
+    {
+        this.gameObject.SetActive(false);
+    }
+
+    public void OnDisable()
+    {
+        ComponentHandler.componentHandler.Unlock();
     }
 }
